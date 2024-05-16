5b7bbb2347e87d1a34e2fccc3c6d62af744d0c64
minhthang29 <minhthang32095@gmail.com>
2024-05-16 10:47:30 +0700

    update

--- test_ops.py
+++ test_ops.py
@@ -1,3 +1,13 @@
 from ops import *
 def test_add():
     assert add(2,3) == 5
+    
+def test_subtract():
+    assert sub(2,3)  == -1
+    
+    
+def test_multiply():
+    assert multiply(2,3) == 5
+    
+def test_divide():
+    assert divide(10,5)  == 2 
